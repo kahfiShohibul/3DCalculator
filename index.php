ece82ee9186dd3584c7708ba28257aeb90c77ceb
kahfiShohibul <79955873+kahfiShohibul@users.noreply.github.com>
2021-03-26 16:43:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content= "width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="styletugas.css">
+    <link rel="stylesheet" type="text/css" href="3dcalculator.css">
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
 
     <title>3D Calculator</title>
